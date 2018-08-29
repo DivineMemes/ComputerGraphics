776cb640bc758b4dc2ed6a92e5b7fbf6bd69429d
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-08-28 17:53:53 -0700

    Added hadoken

--- Assets/MyAssets/Scripts/AnimationHandles/AnimationController.cs
+++ Assets/MyAssets/Scripts/AnimationHandles/AnimationController.cs
@@ -4,24 +4,57 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+
+    //Hadoken particle stuff
+    [SerializeField]
+    ParticleSystem HadokenStartup;
+    [SerializeField]
+    ParticleSystem HadokenBlast;
+
+
+    //Lightning particle stuff
+    [SerializeField]
+    ParticleSystem LeftSparkle;
+    [SerializeField]
+    ParticleSystem RightSparkle;
+    [SerializeField]
+    ParticleSystem LeftLightningSyst;
+    [SerializeField]
+    ParticleSystem RightLightningSyst;
+
+
+
+
+
     public bool Idle = true;
     public Animator anim;
     public bool WaitForFireb;
     public bool WaitForLightningb;
-    public bool WaitForHadukenb;
     public bool WaitForIceb;
+    public bool WaitForHadokenb;
     public bool WaitForSummonb;
     public bool IsInAnimation;
+
+
+
+
+    public bool startLightningParticle;
+
 	void Start ()
     {
         anim = GetComponent<Animator>();
-	}
+
+        //LeftSparkle = transform.Find("LeftSparklingHand");
+        //RightSparkle = transform.Find("RightSparklingHand");
+        //LeftLightningSyst = transform.Find("LeftLightningSystem");
+        //RightLightningSyst = transform.Find("RightLightningSystem");
+    }
 	
 	void Update ()
     {
         bool fireball = Input.GetKeyDown(KeyCode.Alpha1);
         bool Lightning = Input.GetKeyDown(KeyCode.Alpha2);
-        bool Haduken = Input.GetKeyDown(KeyCode.Alpha3);
+        bool Hadoken = Input.GetKeyDown(KeyCode.Alpha3);
         bool Iceblast = Input.GetKeyDown(KeyCode.Alpha4);
         bool Summon = Input.GetKeyDown(KeyCode.Alpha5);
 
@@ -32,21 +65,32 @@ public class AnimationController : MonoBehaviour
                 StartCoroutine(WaitForFire());
                 anim.SetTrigger("Fireball");
             }
+
             if (Lightning && !WaitForLightningb)
             {
                 StartCoroutine(WaitForLightning());
                 anim.SetTrigger("Lightning");
+                LeftSparkle.Play();
+                RightSparkle.Play();
+                StartCoroutine(LightningParticle());
             }
-            if (Haduken && !WaitForHadukenb)
-            {
-                StartCoroutine(WaitForHaduken());
-                anim.SetTrigger("Haduken");
-            }
+
             if (Iceblast && !WaitForIceb)
             {
-                StartCoroutine(WaitForIce());
+                StartCoroutine(WaitForIceBlast());
                 anim.SetTrigger("IceBlast");
             }
+
+            if (Hadoken && !WaitForHadokenb)
+            {
+                StartCoroutine(WaitForHadoken());
+                anim.SetTrigger("Hadoken");
+                StartCoroutine(HadokenParticleStartup());
+                StartCoroutine(HadokenBlastParticle());
+
+
+            }
+
             if (Summon && !WaitForSummonb)
             {
                 StartCoroutine(WaitForSummon());
@@ -77,23 +121,46 @@ public class AnimationController : MonoBehaviour
         WaitForLightningb = true;
         yield return new WaitForSeconds(4.300f);
         WaitForLightningb = false;
+        startLightningParticle = false;
+        LeftSparkle.Stop();
+        RightSparkle.Stop();
         IsInAnimation = false;
     }
 
-    IEnumerator WaitForHaduken()
+    IEnumerator LightningParticle()
+    {
+        yield return new WaitForSeconds(1.067f);
+            LeftLightningSyst.Play();
+            RightLightningSyst.Play();
+    }
+
+
+    IEnumerator WaitForHadoken()
     {
         IsInAnimation = true;
-        WaitForHadukenb = true;
-        yield return new WaitForSeconds(3.333f);
-        WaitForHadukenb = false;
+        WaitForHadokenb = true;
+        yield return new WaitForSeconds(2.633f);
+        WaitForHadokenb = false;
+        HadokenStartup.Stop();
+        HadokenBlast.Stop();
         IsInAnimation = false;
     }
 
-    IEnumerator WaitForIce()
+    IEnumerator HadokenParticleStartup()
+    {
+        yield return new WaitForSeconds(1.033f);
+        HadokenStartup.Play();
+    }
+    IEnumerator HadokenBlastParticle()
+    {
+        yield return new WaitForSeconds(1.567f);
+        HadokenBlast.Play();
+    }
+    IEnumerator WaitForIceBlast()
     {
         IsInAnimation = true;
         WaitForIceb = true;
-        yield return new WaitForSeconds(2.633f);
+        yield return new WaitForSeconds(3.333f);
         WaitForIceb = false;
         IsInAnimation = false;
     }
